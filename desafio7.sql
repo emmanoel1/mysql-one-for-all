c54ce3acb381724912cd805e7d8a0c93b33fb86c
emmanoel <emmanoelporto@gmail.com>
2022-03-14 04:29:15 -0300

    feat: req7

---
+++ desafio7.sql
@@ -0,0 +1,8 @@
+SELECT artist.artist AS artista,
+album.album AS album,
+COUNT(artist_follower.artist_id) AS seguidores
+FROM SpotifyClone.artist
+INNER JOIN SpotifyClone.album ON artist.artist_id = album.artist_id
+INNER JOIN SpotifyClone.artist_follower ON artist_follower.artist_id = artist.artist_id
+GROUP BY artist.artist_id, album.album
+ORDER BY seguidores DESC;
