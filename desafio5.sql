d7325f581601c0468807ec48f76089518436fdfa
emmanoel <emmanoelporto@gmail.com>
2022-03-14 03:29:04 -0300

    fix: req5 define top2 list

--- desafio5.sql
+++ desafio5.sql
@@ -3,4 +3,5 @@ COUNT(history.song_id) AS reproducoes
 FROM SpotifyClone.song
 INNER JOIN SpotifyClone.history
 ON history.song_id = song.song_id
-GROUP BY song.song;
+GROUP BY song.song
+ORDER BY reproducoes DESC LIMIT 2;
