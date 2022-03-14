5d270ae04128187635f0ecc159dc7fde1b2185d5
emmanoel <emmanoelporto@gmail.com>
2022-03-14 04:35:20 -0300

    fix: req7

--- desafio7.sql
+++ desafio7.sql
@@ -2,7 +2,9 @@ SELECT artist.artist AS artista,
 album.album AS album,
 COUNT(artist_follower.artist_id) AS seguidores
 FROM SpotifyClone.artist
-INNER JOIN SpotifyClone.album ON artist.artist_id = album.artist_id
-INNER JOIN SpotifyClone.artist_follower ON artist_follower.artist_id = artist.artist_id
+INNER JOIN SpotifyClone.album
+ON artist.artist_id = album.artist_id
+INNER JOIN SpotifyClone.artist_follower
+ON artist_follower.artist_id = artist.artist_id
 GROUP BY artist.artist_id, album.album
-ORDER BY seguidores DESC;
+ORDER BY seguidores DESC, artista, album;
